4d5e5caa9d7cd6078149445f34fcc621409337dc
Eir03 <staroverovd0@google.com>
2024-10-12 14:08:49 +0300

    Прочие обновления

--- hash_generator/main.py
+++ hash_generator/main.py
@@ -3,13 +3,20 @@ import redis
 import uuid
 from threading import Thread
 import time
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+AUTH_DB_HOST = os.environ.get("AUTH_DB_HOST")
 app = FastAPI(description="hash generator")
 
 # Подключение к Redis
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis(os.environ.get("HOST_REDIS"), 
+                port=os.environ.get("PORT_REDIS"), 
+                db=os.environ.get("DB_REDIS"))
 
-HASH_CACHE_KEY = "hash_cache"  # Ключ для хранения хешей в кэше
+HASH_CACHE_KEY = os.environ.get("HASH_CACHE_KEY")  # Ключ для хранения хешей в кэше
+MAX_COUNT = os.environ.get("MAX_COUNT")
 
 def generate_hash():
     return str(uuid.uuid4())[10:].replace('-','')
@@ -17,8 +24,8 @@ def generate_hash():
 # Функция для наполнения кэша хешами
 def populate_cache():
     while True:
-        if r.llen(HASH_CACHE_KEY) < 1000:
-            for _ in range(1000):
+        if r.llen(HASH_CACHE_KEY) < MAX_COUNT:
+            for _ in range(MAX_COUNT):
                 r.lpush(HASH_CACHE_KEY, generate_hash()) 
         time.sleep(10)  # Спим 10 секунд перед следующей проверкой
 
@@ -26,12 +33,9 @@ def populate_cache():
 Thread(target=populate_cache, daemon=True).start()
 
 
-
 # Endpoint для получения хеша
 @app.get("/get-hash")
 async def get_hash():
     if r.llen(HASH_CACHE_KEY) == 0:  # Если кэш пуст
-        # print('Без кеша')
         return {"hash": generate_hash()}
-    # print('C кешом')
     return {"hash": r.rpop(HASH_CACHE_KEY).decode("utf-8")}  # Возвращаем хеш из кэша
