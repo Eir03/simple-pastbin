1a707e9ee01b65a4c6891b91c91681bcb3e22e2e
Eir03 <staroverovd0@google.com>
2025-03-05 14:33:23 +0300

    Добавил чтение заметок с пагинацией

--- api/src/posts/posts.py
+++ api/src/posts/posts.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timezone
 from hash_gen.hash_gen import get_hash
 from fastapi import APIRouter, Depends, HTTPException
-import httpx
+from typing import List
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from config import URL_BLOB, BUCKET, USE_BLOB
+from config import BUCKET, USE_BLOB
 from posts.blob import upload_text_to_s3, delete_object_from_s3
 from posts.database import Post, get_async_session
 from posts.models import PostCreate, PostRead
@@ -14,11 +14,29 @@ router_post = APIRouter(
     tags=['posts']
 )
 
-@router_post.get('')
-async def get_posts():
-    # Добавить приватность
-    return ''
+@router_post.get('', response_model=List[PostRead])
+async def get_posts(session: AsyncSession = Depends(get_async_session), skip: int = 0, limit: int = 10):
+    query = select(Post).where(Post.is_public == True).offset(skip).limit(limit)
+    result = await session.execute(query)
+    posts = result.scalars().all()
+    return [PostRead.model_validate(post) for post in posts]
 
+@router_post.get('/{hash_id}', response_model=PostRead)
+async def get_post(hash_id: str, session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Post).where(Post.hash == hash_id))
+    result = result.scalars().first()
+    if result is None:
+        raise HTTPException(status_code=404, detail="Not Found")
+    
+    if result.delete_after_reading:
+        await session.delete(result)
+        await session.commit()
+        
+        if USE_BLOB:
+            delete_object_from_s3(BUCKET, result.hash)
+
+    return PostRead.model_validate(result)
+    
 @router_post.post('', response_model=PostRead)
 async def create_post(post: PostCreate, session: AsyncSession = Depends(get_async_session)):
     hash_value = await get_hash()
@@ -49,26 +67,6 @@ async def create_post(post: PostCreate, session: AsyncSession = Depends(get_asyn
     await session.commit()
     await session.refresh(new_post)
     return PostRead.model_validate(new_post)
-
-@router_post.get('/{hash_id}', response_model=PostRead)
-async def get_post(hash_id: str, session: AsyncSession = Depends(get_async_session)):
-    # Добавить удаление после прочтения
-    
-
-    result = await session.execute(select(Post).where(Post.hash == hash_id))
-    result = result.scalars().first()
-    if result is None:
-        raise HTTPException(status_code=404, detail="Not Found")
-    
-    if result.delete_after_reading:
-        await session.delete(result)
-        await session.commit()
-        
-        if USE_BLOB:
-            delete_object_from_s3(BUCKET, result.hash)
-            
-    return PostRead.model_validate(result)
-    
 
 @router_post.put('/{hash_id}')
 async def update_post(hash_id: str, session: AsyncSession = Depends(get_async_session)):
