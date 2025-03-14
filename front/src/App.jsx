7598297b2482f9a36439fa7ffb9fe58a66f96550
Eir03 <staroverovd0@google.com>
2025-03-14 20:40:53 +0300

    Добавил роутеры, создал компонент страницы заметки

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,20 @@
-import './index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PastPage from './components/PastPage'
 import Header from './components/Header';
 import Body from './components/Body';
+import './index.css'
+
 function App() {
   return (
-    <>
-    <Header/>
-    <Body/>
-    </>
+    <Router>
+      <Header/>
+      <Routes>
+        {/* Главная страница */}
+        <Route path="/" element={<Body />} />
+        {/* Настройка маршрута с параметром :id */}
+        <Route path="/:id" element={<PastPage />} />
+      </Routes>
+    </Router>
   );
 }
 
