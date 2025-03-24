087711bf0f7f055b91f4d39ffad5e23458d3d887
Eir03 <staroverovd0@google.com>
2025-03-24 14:40:03 +0300

    сделал небольшой набросок шапки страницы просмотра

--- front/src/components/PastPage.jsx
+++ front/src/components/PastPage.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const PastPage = () => {
   return (
-    <div>PastPage</div>
+    <div className='main h-96 border-solid border-[1px] border-[#ebebeb]'>
+      <div className="m-5 ">
+        <div className="flex items-center gap-5">
+          <img src="" alt="Пользователь" />
+          <p>Title</p>
+        </div>
+        
+        <div className="flex gap-4 items-center">
+          <div className="flex gap-2 items-center">
+            <img src="" alt="Пользователь" />
+            <p>User</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <img src="" alt="Дата" />
+            <p>Date</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <img src="" alt="Кол-во просмотров" />
+            <p>View</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <img src="" alt="Таймер"/>
+            <p>Time delete</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
